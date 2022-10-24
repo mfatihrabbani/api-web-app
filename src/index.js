80788a4f5c6d55c29e8b5b2cbf512ebba2ed27b6
mfatihrabbani <mfatihrabbani@gmail.com>
2022-10-24 18:39:12 +0700

    adding express.json()

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import express from "express"
 import bodyParser from "body-parser"
+import usersApi from "./routes/usersRoute.js"
 
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.json())
+app.use(bodyParser.json())
 app.use("/api/users", usersApi)
 
+app.use("/", (err, req, res, next) => {
+    if(err){
+        console.log(err)
+        const errorMessage = err.message || "Something Error"
+        const statusCode = err.statusCode || 500
+        res.status(statusCode).json({status: "Failed", message: errorMessage}) 
+    }
+})
 
 app.listen(3001, () => {
     console.log("server start..")

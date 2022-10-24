c765c9221e50343b6d73317da43b554ca2a7eb1d
mfatihrabbani <mfatihrabbani@gmail.com>
2022-10-24 18:41:25 +0700

    adding routing api users

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,6 +1,9 @@
 import  express from "express";
+import { loginUser, registerUser } from "../controllers/usersController.js";
 
 const router = express.Router()
 
 router.post("/login", loginUser)
 router.post("/register", registerUser)
+
+export default router

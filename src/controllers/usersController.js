9a02b44c0583a79081a5d7101ef2d755a6c66605
mfatihrabbani <mfatihrabbani@gmail.com>
2022-10-27 21:26:05 +0700

    edit controller

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -16,10 +16,11 @@ export const loginUser = async (req, res, next) => {
 export const registerUser = async (req, res, next) => {
     const {email, password, confirmPassword} = req.body
     const user = {email, password, confirmPassword}
+    console.log(user)
     try {
         const result = await registerUserService(user)
         if(result.status !== undefined) return next(result)
-        res.cookie("Authorization", result.token)
+        res.cookies("Authorization", result.token)
         res.status(200).json({status : "Success", data : result})
     } catch (error) {
         return next(error)
--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import express from "express"
 import bodyParser from "body-parser"
 import usersApi from "./routes/usersRoute.js"
-
+import cors from "cors"
 const app = express()
+
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json())
 app.use(bodyParser.json())
+app.use(cors())
 app.use("/api/users", usersApi)
 
 app.use("/", (err, req, res, next) => {
     if(err){
         console.log(err)
         const errorMessage = err.message || "Something Error"
-        const statusCode = err.statusCode || 500
+        const statusCode = err.status || 500
         res.status(statusCode).json({status: "Failed", message: errorMessage}) 
     }
 })
